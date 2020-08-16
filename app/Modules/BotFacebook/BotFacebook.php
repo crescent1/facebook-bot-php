f820024b4eeea66dd78986041de63ae43a39af93
crescent1 <szuhri1@gmail.com>
2020-08-16 10:48:22 +0700

    perbaikan

--- app/Modules/BotFacebook/BotFacebook.php
+++ app/Modules/BotFacebook/BotFacebook.php
@@ -2,7 +2,57 @@
 
 namespace App\Modules\BotFacebook;
 
+use GuzzleHttp\Client;
+
 class BotFacebook
 {
+    /**
+     * set $http
+     *
+     * @var \GuzzleHttp\Client $http
+     */
+    private $http;
 
+    /**
+     * set token
+     *
+     * @var mixed|\Illuminate\Config\Repository
+     */
+    protected $token;
+
+    public function __construct()
+    {
+        $this->token = config('botfacebook.fb_token');
+
+        $this->http = new Client([
+            'base_uri' => config('botfacebook.base_uri'),
+        ]);
+
+    }
+
+    /**
+     * Kirim pesan dengan menggunakan method messages
+     *
+     * @param array $message
+     * @return void
+     */
+    public function messages(array $message)
+    {
+        $this->http->post('messages?access_token=' . $this->token, [
+            'form_params' => $message,
+        ]);
+    }
+
+    /**
+     * Kirim pesan dengan menggunakan methd messageProfile
+     *
+     * @param array $message
+     * @return void
+     */
+    public function messagesProfile(array $message)
+    {
+        $this->http->post('messenger_profile?access_token=' . $this->token, [
+            'form_params' => $message
+        ]);
+    }
 }
--- config/botfacebook.php
+++ config/botfacebook.php
@@ -2,4 +2,5 @@
 
 return [
     'fb_token' => env('FACEBOOK_TOKEN'),
+    'base_uri' => env('FACEBOOK_BASE_URI'),
 ];
